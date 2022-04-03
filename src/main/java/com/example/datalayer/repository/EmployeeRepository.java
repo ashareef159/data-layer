e9639e686354eec5bf6a9672b4d0e17b0446de4a
alishreef <alishreef30@gmail.com>
2022-04-04 01:38:01 +0300

    N+1 solution 02

--- src/main/java/com/example/datalayer/repository/EmployeeRepository.java
+++ src/main/java/com/example/datalayer/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
-    @EntityGraph(attributePaths = {"department"})
+    //@EntityGraph(attributePaths = {"department"})
+    @Query("from Employee e join Department d on d = e.department")
     public List<Employee> findByFirstName(String name);
 }
